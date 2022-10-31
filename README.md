228a5382dc7c557c092b64837b9817bba50e068f
Misato15 <salvadorchinchilla03@gmail.com>
2022-10-30 19:27:03 -0600

    Servidor básico HTTP utilizando express

---
+++ README.md
@@ -0,0 +1,12 @@
+## Laboratorio 1
+
+# Programación IV
+# Salvador Alejandro Chinchilla Alvarado
+# 21741179
+
+Instrucciones:
+1. Clonar Poryecto
+2. Instalar dependencias
+            Npm install
+3. Ejecutar  proyecto:       
+            Npm start         
---
+++ app.js
@@ -0,0 +1,14 @@
+require ('dotenv').config()
+const express = require ('express')
+const app = express()
+
+app.get('/', function(req,res){
+
+    res.send('Hello World')
+
+})
+
+app.listen(process.env.PORT, ()=>
+{console.log("El puerto está corriendo el puerto TCP" + process.env.PORT)
+
+})
